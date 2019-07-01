1000bdded9e86a025be54b13e3681df10fee9e7c
sauravmaropost <saurav.kumar@maropost.com>
2019-07-01 18:41:09 +0530

    JSMPJ first commit

---
+++ libvue/src/router.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Register from './views/Register.vue'
+import Login from './views/Login.vue'
+import Contact from './views/Contact.vue'
+import User from './views/User.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: User
+    },
+  ]
+})
---
+++ libvue/src/views/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="navbar is-info">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="#">
+        <img
+          src="../../public/logo.png"
+          alt="Bulma: a modern CSS framework based on Flexbox"
+          width="112"
+          height="28"
+        />
+      </a>
+      <div class="navbar-burger burger" @click="showNav =! showNav" data-target="navbarExampleTransparentExample">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div id="navbarExampleTransparentExample" class="navbar-menu" :class="{'is-active':showNav}">
+      <div class="navbar-start">
+        <a class="navbar-item" href="#">Home</a>
+        <a class="navbar-item" href="#">About</a>
+        <a class="navbar-item" href="#">Contacts</a>
+        <a class="navbar-item" href="#">Books</a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link" href="#">Account</a>
+          <div class="navbar-dropdown is-boxed">
+            <a class="navbar-item" href="#">Login</a>
+            <a class="navbar-item" href="#">SignUp</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="field is-grouped">
+            <p class="control">
+              <b-field>
+                <b-input placeholder="Search..." type="search" icon="magnify"></b-input>
+                <p class="control">
+                  <button class="button is-info">Search</button>
+                </p>
+              </b-field>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  
+</template>
+<script>
+export default {
+  data(){
+    return{
+      showNav:false
+    }
+  }
+}
+</script>
+
+
